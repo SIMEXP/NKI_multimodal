602a3954f38ad7c269b7d3914c5707cc74f921b6
AmanPreet Badhwar <amanpreet.badhwar@mail.mcgill.ca>
2017-05-10 14:55:08 -0400

    updated to T33 temporal roi

--- NKI_multimodal_connectome_script.m
+++ NKI_multimodal_connectome_script.m
@@ -19,7 +19,8 @@ files_in = niak_grab_fmri_preprocess(path_preproc, opt_grab);
 % Add brain parcels to files_in structure
 %files_in.network = '/gs/project/gsf-624-aa/database2/preventad/templates/template_cambridge_basc_multiscale_sym_scale007.mnc.gz';
 %files_in.network = '/gs/project/gsf-624-aa/database2/preventad/templates/brain_parcellation_mcinet_basc_sym_77rois.mnc';
-files_in.network = '/gs/project/gsf-624-aa/database2/preventad/templates/brain_parcellation_mcinet_basc_sym_51rois.mnc.gz';
+%files_in.network = '/gs/project/gsf-624-aa/database2/preventad/templates/brain_parcellation_mcinet_basc_sym_51rois.mnc.gz';
+files_in.network = '/gsf-624-aa/database2/preventad/templates/tam_template_2016/template_mcinet_basc_sym_clusters_mnc/brain_parcellation_mcinet_basc_sym_33clusters.mnc.gz';
 
 %%% LIST OF SEEDS
 % The next step is to generate a list of seeds. This requires creating a text file.
@@ -27,22 +28,26 @@ files_in.network = '/gs/project/gsf-624-aa/database2/preventad/templates/brain_p
 
 %files_in.seeds = [path_preproc filesep 'list_seeds.csv'];
 %files_in.seeds = [path_preproc filesep 'list_seeds_T77.csv'];
-files_in.seeds = [path_preproc filesep 'list_seeds_T51.csv'];
+%files_in.seeds = [path_preproc filesep 'list_seeds_T51.csv'];
+files_in.seeds = [path_preproc filesep 'list_seeds_T33.csv'];
 %opt_csv.labels_x = { 'MOTOR' , 'DMN' }; % The labels for the network
 %opt_csv.labels_x = { 'R77_aDMN_1' , 'R77_aDMN_2' }; % The labels for the network
-opt_csv.labels_x = { 'R51_vTL_1' , 'R51_vTL_2' }; % The labels for the network
+%opt_csv.labels_x = { 'R51_vTL_1' , 'R51_vTL_2' }; % The labels for the network
+opt_csv.labels_x = { 'R33_dTL_1' }; % The labels for the network
 opt_csv.labels_y = { 'index' };
 %tab = [3 ; 5];
 %tab = [21 ; 22];
-tab = [16 ; 17];
+%tab = [16 ; 17];
+tab = [12];
 niak_write_csv(files_in.seeds,tab,opt_csv);
 
 %%% SET PIPELINE OPTIONS
 
 % Setup where to store the date
 %opt.folder_out = [path_root 'connectome_test'];
-%opt.folder_out = [path_root 'connectome_T51_20170509_tfemcf'];
-opt.folder_out = [path_root 'connectome_T51_20170510_tfemcf_troi'];
+%opt.folder_out = [path_root 'connectome_T77_20170509_tfemcf'];
+%opt.folder_out = [path_root 'connectome_T51_20170510_tfemcf_troi'];
+opt.folder_out = [path_root 'connectome_T33_20170510_tfemcf_troi'];
 
 % Set options such that we will not generate graph properties, just the correlation maps:
 opt.flag_p2p = false; % No parcel-to-parcel correlation values
